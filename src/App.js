df493c0965239b19b087889a94916749006ee14f
Your Username <user@xample.com>
2024-02-05 16:50:16 -0500

    added react cmponents

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/Landing_Page';
-import Layout from './components/Landing_Page/Landing_page';
+
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
 function App() {
