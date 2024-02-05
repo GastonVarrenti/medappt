1e688be673ae426027f89bfbe941ac8cb7bef346
Your Username <user@xample.com>
2024-02-05 16:40:31 -0500

    added react cmponents

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from './Components/Navbar/Navbar';
+import Landing_Page from './Components/Landing_Page/Landing_Page';
+import Layout from './components/Landing_Page/Landing_page';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
+import Login from './Components/Login/Login';
 function App() {
 
   return (
@@ -10,6 +14,10 @@ function App() {
         <BrowserRouter>
           <Navbar/>
               <Routes>
+              <Route path="/" element={<Landing_Page/>}/>
+              <Route path="/Landing_page" element={<Landing_Page />} />
+              <Route path="/Sign_Up" element={<Sign_Up />} />
+              <Route path="/Login" element={<Login />} />
               </Routes>
             
         </BrowserRouter>
